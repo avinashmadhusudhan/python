c4cd068edf7bb0bfc69b5954f7ccf113b983d99c
Avinash <35699098+avinashmadhusudhan@users.noreply.github.com>
2020-06-25 17:30:48 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,8 +1,6 @@
 import psycopg2
 import numpy as np
 
-
-
 def Cp(mylist, usl, lsl):
             arr = np.array(mylist)
             arr = arr.ravel()
@@ -20,16 +18,13 @@ def Cpk(mylist, usl, lsl):
     Cpl = float(m - lsl) / (3 * sigma)
     Cpk = np.min([Cpu, Cpl])
     return Cpk
-
-
+####
 conn = psycopg2.connect(database="demo", user="postgres", password="admin", host="127.0.0.1",
                         port="5433")
 print("Opened database successfully")
-
 cur = conn.cursor()
 # cur.execute("SELECT value, low_lim from report_data where start")
-
-postgres_insert_query = """ SELECT value, low_lim ,up_lim from ddata between start_date = %s and end_date = %s)"""
+ostgres_insert_query = """ SELECT value, low_lim ,up_lim from ddata between start_date = %s and end_date = %s)"""
 record_to_insert = (start_date, end_date)
 print(start_date, end_date)
 cur.execute(postgres_insert_query, record_to_insert)
@@ -39,7 +34,6 @@ for row in rows:
     a.append(row[0])
 print(a)  # call the cp cpk function
 conn.close()
-
 #######
 print(Cp(a,usl,lsl))
 print(Cpk(a,usl,lsl))
